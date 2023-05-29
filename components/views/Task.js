ceb7fb1968a4304a0b3356202769dcf13eca1748
cplazajr <cesarplazajr@gmail.com>
2023-05-29 14:06:56 -0400

    Capstone-

--- components/views/Task.js
+++ components/views/Task.js
@@ -1,37 +1,53 @@
 import html from "html-literal";
 
-export default () => html`
+export default state => html`
   <div class="blob">
     <div class="task-app">
       <h2 class="dash">Dashboard</h2>
       <form id="taskForm" class="row" method="POST">
-        <input
-          type="text"
-          id="name"
-          name="name"
-          placeholder="Add your text"
-          required
-        />
-        <input
-          type="text"
-          id="description"
-          name="description"
-          placeholder="Add your text"
-          required
-        />
-        <input
-          type="text"
-          id="status"
-          name="status"
-          placeholder="Add your text"
-          required
-        />
-        <button type="submit" name="submit">
-          <i class="fa-solid fa-circle-plus"></i>
-        </button>
+        <div>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Name of task"
+            required
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            id="description"
+            name="description"
+            placeholder="Description of task"
+            required
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            id="status"
+            name="status"
+            placeholder="Status"
+            required
+          />
+        </div>
+        <input id="submit" type="submit" name="submit" value="Create Task" />
       </form>
-      <ul id="list-box"></ul>
-      </div>
+
+      <table id="taskData">
+        <tr>
+          <th>Task Name</th>
+          <th>Task Description</th>
+          <th>Task Status</th>
+        </tr>
+        ${state.tasks
+          .map(Task => {
+            return `<tr><td>${Task.name}</td><td>${Task.description}</td><td>${Task.status}
+        }</td></tr>`;
+          })
+          .join("")}
+      </table>
     </div>
   </div>
 `;
